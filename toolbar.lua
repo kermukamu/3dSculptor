3f1a1184887a8bb50f7fc91d5d887c3745e4636d
kermukamu <juho.kerminen@aalto.fi>
2026-02-01 21:12:28 +0200

    Added submode icons, changed how point collision within toolbar area is defined

--- toolbar.lua
+++ toolbar.lua
@@ -10,15 +10,18 @@ function Toolbar.new(x, y, iconSize, host)
     local iconSelection = love.graphics.newImage(path .. "selection.png")
     local iconMove = love.graphics.newImage(path .. "move.png")
     local iconVertex = love.graphics.newImage(path .. "vertex.png")
+    local iconCircle = love.graphics.newImage(path .. "circle.png")
+    local iconSphere = love.graphics.newImage(path .. "sphere.png")
 
     self.modes = {
-        ["selection"] = iconSelection,
-        ["move"] = iconMove,
-        ["vertex"] = iconVertex
+        ["selection"] = {{"rectangle", iconSelection}},
+        ["move"] = {{"move", iconMove}},
+        ["vertex"] = {{"single", iconVertex}, {"circle", iconCircle}, {"sphere", iconSphere}}
     }
 
     self.x = x
     self.y = y
+    self.drawnIconPositions = {} -- In format {{x1, y1, {mode, subMode}, {x2, y2, {mode, subMode}}...}
     self.size = iconSize
 
     -- Other
@@ -31,17 +34,33 @@ function Toolbar:update(dt)
 end
 
 function Toolbar:draw()
+    self.drawnIconPositions = {}
     local i = 0
+    local currentToolMode = self.host:getToolMode()
+    local currentSubMode = self.host:getSubToolMode()
     for k, v in pairs(self.modes) do
-        local x = self.x
         local y = self.y + self.size*(i)
-        love.graphics.setColor(1,1,1,1) -- White
-        if self.host:getToolMode() == k then love.graphics.setColor(0.5,0.5,0.5,1) end -- Gray
-        love.graphics.rectangle("fill", x, y, self.size, self.size)
-        love.graphics.setColor(0,0,0,1) -- Black
-        love.graphics.rectangle("line", x, y, self.size, self.size)
-        local iconScale = self.size/v:getWidth()
-        love.graphics.draw(v, x, y, 0, self.iconScale, self.iconScale)
+        local x = self.x
+        if k == currentToolMode then -- If selected draw all sub icons next to each other
+            for j = 1, #v, 1 do
+                local subMode = v[j][1]
+                local subIcon = v[j][2]
+                x = self.x - (j-1)*self.size
+                love.graphics.setColor(1,1,1,1) -- White
+                if subMode == currentSubMode then love.graphics.setColor(0.5,0.5,0.5,1) end -- Gray
+                love.graphics.rectangle("fill", x, y, self.size, self.size)
+                local iconScale = self.size/subIcon:getWidth()
+                love.graphics.draw(subIcon, x, y, 0, iconScale, iconScale)
+                table.insert(self.drawnIconPositions, {x, y, k, v[j][1]})
+            end
+        else
+            local toolIcon = v[1][2] -- Choose first from sub icons
+            love.graphics.setColor(1,1,1,1) -- White
+            love.graphics.rectangle("fill", x, y, self.size, self.size)
+            local iconScale = self.size/toolIcon:getWidth()
+            love.graphics.draw(toolIcon, x, y, 0, iconScale, iconScale)
+            table.insert(self.drawnIconPositions, {x, y, k, v[1][1]})
+        end
         i = i + 1
     end
 end
@@ -53,31 +72,30 @@ function Toolbar:mousePressed(mx, my, button)
 end
 
 function Toolbar:switchToModeWithin(mx, my)
-    local i = 0
-    for k, v in pairs(self.modes) do
-        local y = self.y + self.size*(i)
-        local x = self.x
-        if mx > x and x + self.size > mx and my > y and y + self.size > my then
-            self.host:setToolMode(k)
-            return nil
+    for _, xy in ipairs(self.drawnIconPositions) do
+        local iconX = xy[1]
+        local iconY = xy[2]
+        if (iconX < mx and mx < (iconX + self.size)) and
+            (iconY < my and my < (iconY + self.size)) then
+            self.host:setToolMode(xy[3])
+            self.host:setSubToolMode(xy[4])
         end
-        i = i + 1
     end
+end
+
+function Toolbar:isWithinSection(x, y) -- Check if position is within toolbar, returns a table containing the clicked icon
+    for _, xy in ipairs(self.drawnIconPositions) do
+        local iconX = xy[1]
+        local iconY = xy[2]
+        if (iconX < x and x < (iconX + self.size)) and
+            (iconY < y and y < (iconY + self.size)) then
+            return true
+        end
+    end
+    return false
 end
 
 -- Getters
-
-function Toolbar:getHeight()
-    local h = 0
-    for _ in pairs(self.modes) do
-        h = h + self.size 
-    end
-    return h
-end
-
-function Toolbar:getWidth()
-    return self.size
-end
 
 function Toolbar:getX()
     return self.x
